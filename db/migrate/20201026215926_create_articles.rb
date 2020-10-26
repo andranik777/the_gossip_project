f1adaef5665f235f6c34743e007882282da4b892
andranik777 <andranik.hakabyan42@gmail.com>
2020-10-26 23:38:10 +0100

    first commit

---
+++ db/migrate/20201026215926_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :subject
+      t.text :content
+      t.belongs_to :user, index: true #cette ligne rajoute la référence à la table users
+
+
+      t.timestamps
+    end
+  end
+end
